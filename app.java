3fe30d9a167eb0b8d358a0dcb636df6e769ead4d
samruddhi09052005 <samruddhisk09@gmail.com>
2026-02-28 12:04:40 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,5 +1,13 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Declare and initialize two numbers
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Add the numbers and store the sum in a new variable
+        int sum = num1 + num2;
+        
+        // Print the result to the console
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
